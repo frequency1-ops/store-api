8e3b3c61d8b6fc22c9ffd2ce83aeb7ea85bdc1ef
Salim_Juma_001 <87000813+frequency1-ops@users.noreply.github.com>
2025-09-09 00:22:02 +0300

    Entities created

---
+++ src/main/java/com/tech2tech/store/entities/Address.java
@@ -0,0 +1,12 @@
+package com.tech2tech.store.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class Address {
+
+}
---
+++ src/main/java/com/tech2tech/store/entities/Tag.java
@@ -0,0 +1,5 @@
+package com.tech2tech.store.entities;
+
+public class Tag {
+
+}
