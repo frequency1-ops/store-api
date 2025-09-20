59c4d22c2915cdb1f810621e86228f7a3421aa0d
Salim_Juma_001 <87000813+frequency1-ops@users.noreply.github.com>
2025-09-20 18:30:02 +0300

    products deletion and updation completed

--- src/main/java/com/tech2tech/store/controllers/ProductController.java
+++ src/main/java/com/tech2tech/store/controllers/ProductController.java
@@ -1,15 +1,23 @@
 package com.tech2tech.store.controllers;
 
 import java.util.List;
+import java.util.Locale.Category;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.tech2tech.store.dtos.ProductDto;
 import com.tech2tech.store.entities.Product;
 import com.tech2tech.store.mappers.ProductMapper;
+import com.tech2tech.store.repositories.CategoryRepository;
 import com.tech2tech.store.repositories.ProductRepository;
 
 import lombok.AllArgsConstructor;
@@ -21,6 +29,7 @@ public class ProductController {
     
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
     @GetMapping
     public List<ProductDto> getAllProducts(
         @RequestParam(required=false, name="categoryId") Byte categoryId
@@ -34,6 +43,44 @@ public class ProductController {
         return products.stream().map(productMapper::toDto).toList();
         
     }
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(
+        @RequestBody ProductDto productDto,
+        UriComponentsBuilder uriBuilder
+        ){
+            var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+            if (category == null) {
+                return ResponseEntity.badRequest().build();
+            }
+            var product = productMapper.toEntity(productDto);
+            product.setCategory(category);
+            productRepository.save(product);
+            productDto.setId(product.getId());
+            var uri = uriBuilder.path("/products/{id}").buildAndExpand(productDto.getId()).toUri();
+
+            return ResponseEntity.created(uri).body(productDto);
+        }
+        @PutMapping("/{id}")
+        public ResponseEntity<ProductDto> updateProduct(
+            @PathVariable(name = "id") Long id,
+            @RequestBody ProductDto productDto
+
+        ){
+            var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+            if (category == null) {
+                return ResponseEntity.badRequest().build();
+            }
+
+            var product = productRepository.findById(id).orElse(null);
+            if (product == null){
+                return ResponseEntity.notFound().build();
+            }
+            productMapper.update(productDto, product);
+            productRepository.save(product);
+            productDto.setId(product.getId());
+
+            return ResponseEntity.ok(productDto);
+        }
 
 
 
--- src/main/java/com/tech2tech/store/mappers/ProductMapper.java
+++ src/main/java/com/tech2tech/store/mappers/ProductMapper.java
@@ -2,6 +2,7 @@ package com.tech2tech.store.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import com.tech2tech.store.dtos.ProductDto;
 import com.tech2tech.store.entities.Product;
@@ -10,5 +11,8 @@ import com.tech2tech.store.entities.Product;
 public interface ProductMapper {
     @Mapping(source = "category.id", target = "categoryId")
     ProductDto toDto(Product product);
+    Product toEntity(ProductDto productDto);
+    @Mapping(target = "id", ignore = true)
+    void update(ProductDto productDto, @MappingTarget Product product);
 
 }
